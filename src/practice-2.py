169f7d5309c153325c07888531a9a9b759552d52
DongHeon Lee <niche3600@gmail.com>
2025-08-02 18:41:41 +0900

    コードの関心事をWeworkRemotelyScraperに寄せる

--- src/practice-2.py
+++ src/practice-2.py
@@ -2,17 +2,19 @@ import cloudscraper
 from abc import abstractmethod  # abcモジュールをインポート
 from bs4 import BeautifulSoup
 
-url = "https://weworkremotely.com/categories/remote-full-stack-programming-jobs#job-listings"
+base_url = "https://weworkremotely.com"
+target_path = "/categories/remote-full-stack-programming-jobs#job-listings"
 
 
 class Scraper:
-    def __init__(self, url):
-        self.url = url
+    def __init__(self, base_url, target_path):
+        self.base_url = base_url
+        self.target_path = target_path
         self.job_list = []
 
     def get_content(self):
         scraper = cloudscraper.create_scraper()
-        response = scraper.get(self.url)
+        response = scraper.get(f"{self.base_url}{self.target_path}")
         soup = BeautifulSoup(response.content, "html.parser")
         return soup
 
@@ -22,8 +24,8 @@ class Scraper:
 
 
 class WeworkRemotelyScraper(Scraper):
-    def __init__(self, url):
-        super().__init__(url)
+    def __init__(self, base_url, target_path):
+        super().__init__(base_url, target_path)
 
     def get_jobs(self):
         soup = self.get_content()
@@ -47,11 +49,13 @@ class WeworkRemotelyScraper(Scraper):
                 })
             categories = [category.text for category in categories]
             categories = Util.str_list_to_str(categories)
+            url = self._get_job_url(job.find_all("a"))
             job_data = {
                 "title": title,
                 "company": company,
                 "head_quater": head_quater,
-                "categories": categories
+                "categories": categories,
+                "url": url
             }
             self.job_list.append(job_data)
         return self.job_list
@@ -59,22 +63,30 @@ class WeworkRemotelyScraper(Scraper):
     def print_jobs(self):
         jobs = self.get_jobs()
         for job in jobs:
-            Util.print_job_details(**job)
+            self._print_job_details(**job)
 
+    def _get_job_url(self, url):
+        if not url:
+            return None
+        elif len(url) >= 2:
+            return f"{self.base_url}{url[1]['href']}"
+        else:
+            return f"{self.base_url}{url[0]['href']}"
 
-class Util:
-    @staticmethod
-    def print_job_details(title, company, head_quater, categories):
+    def _print_job_details(self, title, company, head_quater, categories, url):
         print(f"Title: {title}")
         print(f"Company: {company}")
         print(f"Headquarters: {head_quater}")
         print(f"Categories: {categories}")
+        print(f"URL: {url}")
         print("-" * 40)
 
+
+class Util:
     @staticmethod
     def str_list_to_str(str_list):
         return ", ".join(str_list)
 
 
-WeworkRemotelyScraper = WeworkRemotelyScraper(url)
+WeworkRemotelyScraper = WeworkRemotelyScraper(base_url, target_path)
 WeworkRemotelyScraper.print_jobs()

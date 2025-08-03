728757449683f816c465bb44d379083eb58c941e
DongHeon Lee <niche3600@gmail.com>
2025-08-03 20:59:27 +0900

    playwright exec and get content

--- src/practice_5.py
+++ src/practice_5.py
@@ -1,11 +1,43 @@
 from playwright.sync_api import sync_playwright
+import time
+from bs4 import BeautifulSoup
 
 p = sync_playwright().start()
 
-browser = p.chromium.launch()
+browser = p.chromium.launch(headless=False)
+
 page = browser.new_page()
-page.goto("https://google.com")
-print(page.title())
-page.screenshot(path="screenshot.png")
+
+page.goto("https://www.wanted.co.kr/")
+
+time.sleep(7)
+
+page.click('button[aria-controls="nav_searchbar"]')
+
+time.sleep(3)
+
+page.get_by_placeholder("검색어를 입력해 주세요").fill("flutter")
+
+time.sleep(3)
+
+page.keyboard.press("Enter")
+
+time.sleep(10)
+
+page.click("a#search_tab_position")
+
+time.sleep(7)
+
+for x in range(5):
+    time.sleep(5)
+    page.keyboard.down("End")
+
+time.sleep(5)
+
+content = page.content()
+
 browser.close()
+
 p.stop()
+
+soup = BeautifulSoup(content, "html.parser")

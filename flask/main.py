8eb1b0e19c9ee6a899cab9f74b182b2113792cc8
DongHeon Lee <niche3600@gmail.com>
2025-08-10 17:46:04 +0900

    flask 루팅 추가

--- flask/main.py
+++ flask/main.py
@@ -1,9 +1,21 @@
-from extractor.wanted import extract_wanted_jobs
-from modules.wanted_scraper import csv_first_row
-from files import save_to_file
+from flask import Flask
 
-keyword = input('What do you want to search for?')
+app = Flask('JobScrapper')
 
-wanted = extract_wanted_jobs(keyword)
 
-save_to_file(keyword, csv_first_row, wanted)
+@app.route('/')
+def home():
+    return "Welcome to the Job Scrapper!"
+
+
+app.run("0.0.0.0", port=80, debug=True)
+
+# from extractor.wanted import extract_wanted_jobs
+# from modules.wanted_scraper import csv_first_row
+# from files import save_to_file
+
+# keyword = input('What do you want to search for?')
+
+# wanted = extract_wanted_jobs(keyword)
+
+# save_to_file(keyword, csv_first_row, wanted)

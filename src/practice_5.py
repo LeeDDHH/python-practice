60e3b7b37a3ccccafcfbfbe4179b5e74ca092cb1
DongHeon Lee <niche3600@gmail.com>
2025-08-10 14:06:54 +0900

    practice_5 modified

--- src/practice_5.py
+++ src/practice_5.py
@@ -2,6 +2,8 @@ from playwright.sync_api import sync_playwright
 import time
 from bs4 import BeautifulSoup
 
+base_url = "https://www.wanted.co.kr"
+
 p = sync_playwright().start()
 
 browser = p.chromium.launch(headless=False)
@@ -10,7 +12,7 @@ page = browser.new_page()
 
 page.goto("https://www.wanted.co.kr/")
 
-time.sleep(7)
+time.sleep(3)
 
 page.click('button[aria-controls="nav_searchbar"]')
 
@@ -22,11 +24,11 @@ time.sleep(3)
 
 page.keyboard.press("Enter")
 
-time.sleep(10)
+time.sleep(5)
 
 page.click("a#search_tab_position")
 
-time.sleep(7)
+time.sleep(5)
 
 for x in range(5):
     time.sleep(5)
@@ -41,3 +43,20 @@ browser.close()
 p.stop()
 
 soup = BeautifulSoup(content, "html.parser")
+
+jobs = soup.find_all("div", role="listitem")
+
+for job in jobs:
+    url = job.find("a")["href"]
+    title = job.find("strong")
+    company_name = job.find(
+        "span",
+        class_="CompanyNameWithLocationPeriod_CompanyNameWithLocationPeriod__company__ByVLu wds-nkj4w6",
+    )
+    location = job.find("div", class_="job-region")
+
+    # if title and company and location:
+    #     print(f"Title: {title.get_text(strip=True)}")
+    #     print(f"Company: {company.get_text(strip=True)}")
+    #     print(f"Location: {location.get_text(strip=True)}")
+    #     print("-" * 40)

627210f405a7d6cb1ed48316106be746fac1441a
DongHeon Lee <niche3600@gmail.com>
2025-08-10 19:56:01 +0900

    스크래이핑한 결과를 다운로드할 수 있도록 루트와 기능 추가

--- flask/main.py
+++ flask/main.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, send_file
 from extractor.wanted import extract_wanted_jobs
+from modules.wanted_scraper import csv_first_row
+from files import save_to_file
 
 app = Flask('JobScrapper', template_folder='flask/templates')
 
@@ -24,6 +26,19 @@ def search():
         db[keyword] = wanted
 
     return render_template("search.html", keyword=keyword, jobs=wanted)
+
+
+@app.route('/export')
+def export():
+    keyword = request.args.get('keyword')
+    if keyword is None or not keyword:
+        return redirect('/')
+
+    if keyword not in db:
+        return redirect(f"/search?keyword={keyword}")
+
+    save_to_file(keyword, csv_first_row, db[keyword])
+    return send_file(f"./src/files/{keyword}_jobs.csv", as_attachment=True, download_name=f"{keyword}_jobs.csv")
 
 
 app.run("0.0.0.0", port=80, debug=True)
